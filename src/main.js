41530c8d70039a0082fbabf09cbff2f8f5b08b77
Nabufaces <598824163@qq.com>
2017-11-22 17:43:48 +0800

    release 0.0.8

--- src/main.js
+++ src/main.js
@@ -59,6 +59,7 @@ const install = function(Vue, opts = {}) {
   Vue.prototype.$Modal = lanModal;
   Vue.prototype.$Message = lanMessage;
   Vue.prototype.$messageBox = lanMessageBox;
+  console.log(Vue.prototype);
 };
 
 /* istanbul ignore if */
@@ -66,9 +67,10 @@ if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue);
 };
 
+console.log(components);
 
 export default {
-  version: '0.0.5',
+  version: '0.0.8',
   install,
   ...components
 };
