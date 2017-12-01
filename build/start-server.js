592e815f31bff0eaa23d81c9bd094b6654cdc45e
Nabufaces <598824163@qq.com>
2017-12-01 15:05:56 +0800

    update start-server

--- build/start-server.js
+++ build/start-server.js
@@ -7,10 +7,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('dist'));
-app.use(require('connect-history-api-fallback')());
 
 app.all('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('404 NOT FIND!');
+  res.status(500).send('500 SERVER ERROR!');
 });
 
 app.use((err, req, res, next) => {
